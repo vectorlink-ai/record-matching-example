5b75aba155d062a344b90f04da1f12b0edab4af6
Maren van Otterdijk <maren@vectorlink.ai>
2025-01-16 12:52:27 +0100

    add basic dependencies

---
+++ nix/uv-python.nix
@@ -0,0 +1,15 @@
+{pyproject-nix, pyproject-build-systems, pyproject-overrides, cudaPackages, lib, workspace, python3, callPackage}:
+let overlay = workspace.mkPyprojectOverlay {
+      sourcePreference = "wheel";
+    };
+in
+(callPackage pyproject-nix.build.packages {
+  python = python3;
+}).overrideScope (
+  lib.composeManyExtensions [
+    pyproject-build-systems.overlays.default
+    overlay
+    pyproject-overrides.cuda
+    pyproject-overrides.default
+  ]
+)
