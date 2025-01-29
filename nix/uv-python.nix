cfa5f234605a444f2e2db79fe5d630b9a7ede15f
Maren van Otterdijk <maren@vectorlink.ai>
2025-01-29 13:52:36 +0100

    move python uv2nix overrides for building source projects to shared

--- nix/uv-python.nix
+++ nix/uv-python.nix
@@ -2,14 +2,6 @@
 let overlay = workspace.mkPyprojectOverlay {
       sourcePreference = "wheel";
     };
-    fix-vectorlink-gpu-build = final: prev: {
-      vectorlink-gpu = prev.vectorlink-gpu.overrideAttrs (old: {
-        buildInputs = old.buildInputs or [] ++ [final.hatchling final.pathspec final.pluggy final.packaging final.trove-classifiers];
-      });
-      vectorlink-py = prev.vectorlink-py.overrideAttrs (old: {
-        buildInputs = old.buildInputs or [] ++ [final.hatchling final.pathspec final.pluggy final.packaging final.trove-classifiers];
-      });
-    };
 in
 (callPackage pyproject-nix.build.packages {
   python = python3;
@@ -19,6 +11,6 @@ in
     overlay
     pyproject-overrides.cuda
     pyproject-overrides.default
-    fix-vectorlink-gpu-build
+    pyproject-overrides.vectorlink-source-projects
   ]
 )
